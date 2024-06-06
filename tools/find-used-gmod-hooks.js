eff2c7340d5b93b2ebba4ab3ca6f1b75f540ad8c
luttje <2738114+luttje@users.noreply.github.com>
2024-06-06 10:16:21 +0200

    hook finding tool

---
+++ tools/find-used-gmod-hooks.js
@@ -0,0 +1,90 @@
+/*
+    Run this script to scrap your Garry's Mod projects (specifically Helix schema's)
+    to see which hooks are being used in your codebase.
+
+    It'll be outputted to hooks.txt in the same directory as this script.
+
+    E.g:
+    node find-used-gmod-hooks.js "C:\Projects\gmod-experiment-redux" "C:\Projects\helix" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\gamemodes\sandbox" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\gamemodes\base"
+*/
+
+const fs = require('fs');
+const path = require('path');
+
+const targetPatterns = [
+    /function GM:(\w+)\((\w+), (\w+)\)/g,
+    /function Schema:(\w+)\((\w+), (\w+)\)/g,
+    /function PLUGIN:(\w+)\((\w+), (\w+)\)/g,
+    /hook\.Add\("(\w+)", "ignore", (\w+)\)/g,
+];
+
+const outputFile = 'hooks.txt';
+
+function traverseDirectory(dir) {
+    if (dir.includes('node_modules') || dir.includes('.git')) {
+        return;
+    }
+
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            console.error(`Error reading directory ${dir}: ${err.message}`);
+            return;
+        }
+
+        files.forEach(file => {
+            const filePath = path.join(dir, file);
+
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    console.error(`Error reading file stats ${filePath}: ${err.message}`);
+                    return;
+                }
+
+                if (stats.isDirectory()) {
+                    traverseDirectory(filePath);
+                } else if (stats.isFile() && file.endsWith('.lua')) {
+                    findPatternsInFile(filePath);
+                }
+            });
+        });
+    });
+}
+
+function findPatternsInFile(file) {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading file ${file}: ${err.message}`);
+            return;
+        }
+        targetPatterns.forEach(pattern => {
+            let match;
+            while ((match = pattern.exec(data)) !== null) {
+                logMatchToFile(file, match[0]);
+            }
+        });
+    });
+}
+
+function logMatchToFile(file, match) {
+    const logEntry = `${match}\n`;
+    fs.appendFile(outputFile, logEntry, (err) => {
+        if (err) {
+            console.error(`Error writing to log file: ${err.message}`);
+        }
+    });
+}
+
+const startDirectories = process.argv.slice(2);
+
+console.log(`Starting search in directories: ${startDirectories}`);
+
+// Clear the output file before starting
+fs.writeFile(outputFile, '', (err) => {
+    if (err) {
+        console.error(`Error initializing log file: ${err.message}`);
+    } else {
+        startDirectories.forEach(dir => {
+            traverseDirectory(dir);
+        });
+    }
+});
