8ccdf74670a9a1f07d6b83887914c0e91684a1d0
luttje <2738114+luttje@users.noreply.github.com>
2024-06-09 13:59:32 +0200

    show player speaking gesture

--- src/game/shared/hl2mp/hl2mp_playeranimstate.cpp
+++ src/game/shared/hl2mp/hl2mp_playeranimstate.cpp
@@ -13,6 +13,7 @@
 
 #ifdef CLIENT_DLL
 #include "c_hl2mp_player.h"
+#include "voice_status.h"
 #else
 #include "hl2mp_player.h"
 #endif
@@ -361,11 +362,14 @@ Activity CHL2MPPlayerAnimState::CalcMainActivity()
 {
     Activity idealActivity = BaseClass::CalcMainActivity();
 
-    // TODO: If the player is using voice, add the gesture for that
-    if ( false )
+#ifdef CLIENT_DLL
+    // If the player is using voice, add the gesture for that
+    if ( ( m_pPlayer == C_BasePlayer::GetLocalPlayer() && GetClientVoiceMgr()->IsLocalPlayerSpeaking() )
+        || GetClientVoiceMgr()->IsPlayerSpeaking( m_pPlayer->entindex() ) )
     {
         AddToGestureSlot( GESTURE_SLOT_CUSTOM, ACT_GMOD_IN_CHAT, true );
     }
+#endif
 
     return idealActivity;
 }
