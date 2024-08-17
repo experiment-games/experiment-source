a5a0677c4375cb11eade7e4bc23f7da9a28d07ca
luttje <2738114+luttje@users.noreply.github.com>
2024-08-17 14:27:45 +0200

    make noclip not a cheat

--- src/game/server/client.cpp
+++ src/game/server/client.cpp
@@ -1209,7 +1209,7 @@ void CC_Player_NoClip( void )
     }
 }
 
-static ConCommand noclip( "noclip", CC_Player_NoClip, "Toggle. Player becomes non-solid and flies.", FCVAR_CHEAT );
+static ConCommand noclip( "noclip", CC_Player_NoClip, "Toggle. Player becomes non-solid and flies." );
 
 //------------------------------------------------------------------------------
 // Sets client to godmode
